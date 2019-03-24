f97dc42dcb4b591fb5f0b79fc6ca060e5b49e6b3
Heather Herbert <Heather.Herbert.1975@gmail.com>
2019-03-24 22:02:44 +0000

    Setup enviroment and started work

---
+++ WProtection/classes/database.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: heather
+ * Date: 24/03/19
+ * Time: 21:50
+ */
+
+namespace WProtection;
+
+
+class database
+{
+
+
+
+}
---
+++ WProtection/classes/settings.sample.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: heather
+ * Date: 24/03/19
+ * Time: 21:51
+ */
+
+namespace WProtection;
+
+
+class settings
+{
+    public $DB_HOST = "localhost";
+    public $DB_USER = "username";
+    public $DB_PASS = "password";
+
+}
